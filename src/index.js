e9a45a2ab84706f3b3e95d2348b22fb0409ef18c
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-09-09 21:48:16 +0300

    add hashHistory, delete browserHistory

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {createStore, combineReducers} from "redux";
 import {Provider} from "react-redux";
-import {Router, Route, browserHistory} from "react-router";
-import {syncHistoryWithStore, routerReducer} from "react-router-redux";
+import {Router, Route, hashHistory} from "react-router";
+import {routerReducer} from "react-router-redux";
 
 import "./styles/main.css";
 import reducer from "./reducers/";
 import Shop from "./containers/Shop";
 import ItemPage from "./containers/ItemPage";
+import Admin from "./containers/Admin";
 
 
 const store = createStore(
@@ -19,13 +20,13 @@ const store = createStore(
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const history = syncHistoryWithStore(browserHistory, store);
-
 ReactDOM.render(
 	<Provider store={store}>
-		<Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
+		<Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
 			<Route path="/" component={Shop}/>
 			<Route path="/item/:id" component={ItemPage} />
+			<Route path="/admin" component={Admin}/>
 		</Router>
 	</Provider>,
-	document.getElementById("root"));
+	document.getElementById("root")
+);
